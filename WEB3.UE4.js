76aa71f39c75961f34ba4af2107ee9944fe55ccf
AVEWAR <98576359+AVEWAR@users.noreply.github.com>
2022-01-28 07:58:19 +0100

    Update WEB3.UE4.js

--- WEB3.UE4.js
+++ WEB3.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('UE4Game.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://raw.githubusercontent.com/AVEWAR/AVE_Room/main/UE4Game.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
